207c89c87f16162ad16080585d49ad66f85688db
Linda <linda.beaumont@mq.edu.au>
2014-02-19 12:23:48 +1100

    Lots more lines added

--- gapminder.R
+++ gapminder.R
@@ -1,3 +1,8 @@
 data <- read.csv("data/gapminder-FiveYearData.csv", stringsAsFactors=FALSE)
 
 # Subset the data
+# need nature paper
+# This line has changed
+# This line has also changed
+# As has this one
+# OK, no more changes, please
